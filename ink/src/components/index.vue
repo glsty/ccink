c5984035492e4496df937078877626ba88e3c868
gonglong <glstrive@163.com>
2018-01-15 20:05:08 +0800

    缺失

---
+++ ink/src/components/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>测试主页</h1>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  data () {
+    /*return {
+      msg: 'Welcome to Your Vue.js App'
+    }*/
+  }
+}
+</script>
+
